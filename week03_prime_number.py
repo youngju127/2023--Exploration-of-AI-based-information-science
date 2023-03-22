3a5e7a329245dfc1610b2908a09280ea8a4b1f1d
youngju <ignite01270@gmail.com>
2023-03-22 21:48:33 +0900

    week03_prime_number.py_v1.1

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,11 +1,12 @@
-# week03_prime_number v1.0
-numberstart = int(input("input number to start : "))
-numberend = int(input("input number to end : "))
+# week03_prime_number v1.1
+#numberstart = int(input("input number to start : "))
+#numberend = int(input("input number to end : "))
 #count = 0
+numbers = input("Enter starting number and ending number : ").split()
+numberstart = int(numbers[0])
+numberend = int(numbers[1])
 for k in range(numberstart, numberend+1):
-
     is_prime = True
-
     if k < 2:
         is_prime = False
     else:
@@ -13,7 +14,6 @@ for k in range(numberstart, numberend+1):
             if k % i == 0:
                 is_prime = False
                 break #If not prime number, exit the loop as soon as the firstdivisor is found
-
     #if is_prime == True:
     if is_prime:
         print(f"{k}", end=' ')
