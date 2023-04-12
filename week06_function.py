24ef9385a43ba196304f7b6a9f6f6b466f3cf1b1
youngju <ignite01270@gmail.com>
2023-04-12 20:55:00 +0900

    week06_decorator

--- week06_function.py
+++ week06_function.py
@@ -1,30 +1,42 @@
 # week06_function.py
-import time
+
 # def my_generator(first =0, last=10, step=1):
 #     number = first
 #     while number < last:
 #         yield number
 #         number += step
-#
+
 # ranger = my_generator(5,10)
 # for k in ranger:
 #     print(k, end=' ')
 # for k in ranger:
 #     print(k, end=' ')
 
-def factorial(n):
+# s = time.time()
+# t = time.time()
+# print(f'total time is {t - s}second')
 
-    s = time.time()
+import time
+def time_checker(func): #closure
+    def inner(*args):
+        s = time.time()
+        r = func(*args)
+        e = time.time()
+        print(f'total time is {e - s}second')
+        return r
+    return inner
+@time_checker
+def factorial(n):
     result = 1
     for k in range(1, n+1):
         result *= k
-    e = time.time()
-    print(f'total time is {e-s} second')
     return result
-def power(b, e):
-    s = time.time()
-    n = b**e
-    t = time.time()
-    print(f'total time is {t-s}second')
 
-print(factorial(1000))
+@time_checker
+def power(b, e):
+    n = b**e
+    return n
+
+
+print(power(2, 100))
+
