d37916618296105015bb768a2f147c5a721395d1
youngju127 <ignite01270@gmail.com>
2023-05-17 21:42:54 +0900

    week09_class_object_final

--- week09_class_object03.py
+++ week09_class_object03.py
@@ -5,21 +5,35 @@ class Pokemon():
         self.hp = hp
     def say(self):
         print(f"I am pokemon!, My name is {self.name}")
+    def attack(self):
+        print(f"{self.name} launches an area attack")
+    def attack_target(self, target):
+        print(f"{self.name} launches an area attack to {target}")
 class Pikachu(Pokemon): #is-a realtionship
-    pass
-class Squirtle(Pokemon):
-    pass
-class Charizard(Pokemon):
-    pass
+    def attack(self):
+        print(f"{self.name} casts a million volt attack")
 
+    def attack_target(self, target):
+        print(f"{self.name} casts a million volt to {target}")
+class Squirtle(Pokemon):
+    def attack(self):
+        print(f"{self.name} casts a water gun attack")
+
+    def attack_target(self, target):
+        print(f"{self.name} casts a water gun to {target}")
+class Charizard(Pokemon):
+    def attack(self):
+        print(f"{self.name} casts a flamethrower attack")
+
+    def attack_target(self, target):
+        print(f"{self.name} casts a flamethrower to {target}")
 if __name__ =="__main__":
     pikachu = Pikachu("pikachu", 1, 35) # instance creation
     squirtle = Squirtle("squirtle", 1, 44)
     charizard = Charizard("charrizard", 36, 78)
 
-    print(squirtle.name)
-    pikachu.say()
-    print(issubclass(Squirtle, Pokemon))
-    charizard.say()
+    pikachu.attack()
+    squirtle.attack_target(charizard.name)
+    charizard.attack_target(squirtle.name)
 
 
