30b1286af54c5c36826b7aa07f64bec91e03d77a
youngju127 <ignite01270@gmail.com>
2023-05-17 20:41:07 +0900

    week09_class_object02

--- week09_class_object03.py
+++ week09_class_object03.py
@@ -1,11 +1,25 @@
 class Pokemon():
+    def __init__(self, name, level, hp):
+        self.name = name
+        self.level = level
+        self.hp = hp
+    def say(self):
+        print(f"I am pokemon!, My name is {self.name}")
+class Pikachu(Pokemon): #is-a realtionship
     pass
-if __name__ =="__main__":
-    pikachu = Pokemon() # instance creation
-    eevee = Pokemon() # instance creation
-    pikachu.level = 1
-    pikachu.hp = 35
-    pikachu.nemesis = eevee
+class Squirtle(Pokemon):
+    pass
+class Charizard(Pokemon):
+    pass
 
-    print(pikachu.hp)
-    print(pikachu.nemesis)
+if __name__ =="__main__":
+    pikachu = Pikachu("pikachu", 1, 35) # instance creation
+    squirtle = Squirtle("squirtle", 1, 44)
+    charizard = Charizard("charrizard", 36, 78)
+
+    print(squirtle.name)
+    pikachu.say()
+    print(issubclass(Squirtle, Pokemon))
+    charizard.say()
+
+
