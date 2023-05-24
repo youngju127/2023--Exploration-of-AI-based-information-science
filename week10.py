0e109d16715810f2e8b60d3a4c17bbb80295b0ee
youngju127 <ignite01270@gmail.com>
2023-05-24 19:46:32 +0900

    week10

--- week10.py
+++ week10.py
@@ -6,10 +6,30 @@ class PrettyMixin:
 
 class Pokemon(PrettyMixin):
     def __init__(self, name, hp, level):
-        self.name = name
+        self.hidden_name = name
         self.hp = hp
         self.level = level
 
+    def get_name(self):
+        print("getter executed")
+        return self.hidden_name
+    def set_name(self, name):
+        print("setter executed")
+        self.hidden_name = name
+
+    def info(self):
+        print("=============")
+        print(f"Name : {self.hidden_name}")
+        print(f"Hp : {self.hp}")
+        print(f"Level : {self.level}")
+        print("=============")
+
 if __name__ == "__main__":
     p1 = Pokemon("Pikachu", 35, 1)
+    p2 = Pokemon("Squirtle", 40, 1)
     p1.dump()
+    p1.info()
+    p2.level = 2
+    p2.info()
+    p2.set_name("wartotle")
+    print(p2.get_name())
